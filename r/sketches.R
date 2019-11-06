862774e4020fe637daacd10903f3b1dc8c7ad77a
Jason Kao <jason.kao@columbia.edu>
2019-11-05 22:22:30 -0500

    sketches

--- r/sketches.R
+++ r/sketches.R
@@ -4,7 +4,9 @@ library(jsonlite)
 students <- fromJSON("../data/students-110119.json")
 coci_courses <- fromJSON("../data/contemporary_civilization_courses.json")
 
-students %>% 
-  mutate(instructor_uni = uni) %>% 
-  merge(coci_courses, by = "instructor_uni") %>% 
-  View()
+coci_courses %>% 
+  rename(uni = instructor_uni, course_title = title) %>% 
+  merge(students, by = "uni") %>% 
+  ggplot(aes(department)) +
+  geom_bar() +
+  coord_flip()
