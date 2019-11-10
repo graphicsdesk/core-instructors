ff2026d751c0b3559bd07a2e86d5a57e8f6da8c2
pseyla <phanseyla@gmail.com>
2019-11-10 15:51:00 -0500

    commiting changes to lithum_sketch.R

--- r/lithum_sketch.R
+++ r/lithum_sketch.R
@@ -1,13 +1,44 @@
 library(tidyverse)
 library(jsonlite)
+library(magrittr)
+library(dplyr)
+
+# Read in people from directory
 
 students <- fromJSON("../data/students-110119.json")
-lithum_courses <- fromJSON("../data/literature_humanities.json")
+facultyanddstaff <- fromJSON("../data/facultyandstaff-110119.json")
+people <- union(facultyanddstaff, students)
 
-lithum_courses %>%
-  rename(uni = instructors_uni, course_title = title) %>%
-  merge(students, by "uni") %>%
-  ggplot(aes(department)) +
-  geom_bar()+
-  coord_flip()
+# Read in courses and combine them; we will do this in Python
 
+lithum_courses <- fromJSON("../data/literature_humanities.json") %>% 
+  mutate(subject = "Literature Humanities")
+cc_courses <- fromJSON("../data/contemporary_civilization_courses.json") %>% 
+  mutate(subject = "Contemporary Civilization")
+all_courses <- union(lithum_courses, cc_courses)
+
+# Counting and graphing
+
+all_courses %>% 
+  rename(uni = instructor_uni, course_title = title) %>% 
+  merge(people, by = "uni") %>% 
+  mutate(
+    instructor_type = case_when(
+      grepl("Lecturer", title) ~ "Nontenured",
+      grepl("Student", title) ~ "Student",
+      uni %in% c("rcp6", "tsl2119") ~ "Student",
+      grepl("Assistant Professor", title) ~ "Tenure-track",
+      grepl("Professor", title) ~ "Tenured",
+      grepl("Dean", title) ~ "Administrative",
+      TRUE ~ "UNCAUGHT"
+    )
+  ) %>% 
+  count(instructor_type, subject) %>% 
+  group_by(subject) %>% 
+  mutate(
+    pct = n / sum(n)
+  ) %>% 
+  ggplot(aes(instructor_type, pct)) +
+  geom_col() +
+  coord_flip() +
+  facet_wrap(~ subject)
