475842925a2595f1e4d1db5725b937ce48d36666
pseyla <phanseyla@gmail.com>
2019-11-10 16:15:08 -0500

    R file that creates three different graphs - cc, uw, and lithum

---
+++ r/all_core_classes.R
@@ -0,0 +1,42 @@
+library(tidyverse)
+library(jsonlite)
+library(magrittr)
+library(dplyr)
+
+#Read in people from the directory 
+students <- fromJSON("../data/students-110119.json")
+facultyanddstaff <- fromJSON("../data/facultyandstaff-110119.json")
+people <- union(facultyanddstaff, students)
+
+#Read in all courses (uw, cc, and lithum)
+all_courses <- fromJSON("../data/all_courses.json")
+
+
+# Counting and graphing
+
+all_courses %>% 
+  rename(uni = instructor_uni, course_title = title) %>% 
+  merge(people, by = "uni") %>% 
+  mutate(
+    instructor_type = case_when(
+      grepl("Lecturer", title) ~ "Nontenured",
+      grepl("Student", title) ~ "Student",
+      uni %in% c("rcp6", "tsl2119","hck2110") ~ "Student",
+      grepl("Assistant Professor", title) ~ "Tenure-track",
+      grepl("Professor", title) ~ "Tenured",
+      grepl("Dean", title) ~ "Administrative",
+      TRUE ~ "UNCAUGHT"
+    )
+  ) %>% 
+  #filter(instructor_type == "UNCAUGHT") %>%
+  #View()
+  count(instructor_type, subject) %>% 
+  group_by(subject) %>% 
+  mutate(
+    pct = n / sum(n)
+  ) %>% 
+  ggplot(aes(instructor_type, pct)) +
+  geom_col() +
+  coord_flip() +
+  facet_wrap(~ subject)
+
